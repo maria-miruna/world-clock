274f7e8e09b8a5f28910915134fe00b9727f8969
maria-miruna <maria.botis1997@gmail.com>
2023-05-11 17:28:14 +0100

    added the functionality for the select

--- javascript/index.js
+++ javascript/index.js
@@ -12,6 +12,24 @@ function updateTime() {
   });
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  showElement(
+    "#cities",
+    `<div class="col-6 mb-2 pe-0">
+       <div class="city-name">${cityName}</div>
+       <div class="date">${cityTime.format(
+         "MMMM Do YYYY"
+       )}</div> <!-- Removed extra quotation mark -->
+         </div>
+         <div class="col-6 mb-2 time text-end">
+         ${cityTime.format("h:mm:ss [<small>]A[</small>]")}
+         </div>`
+  );
+}
+
 let locations = [
   {
     name: "Los Angeles",
@@ -38,6 +56,9 @@ let locations = [
     timeElement: "#milan-time",
   },
 ];
+
+let citySelectElement = document.querySelector("#city");
+citySelectElement.addEventListener("change", updateCity);
 
 updateTime();
 setInterval(updateTime, 1000);
